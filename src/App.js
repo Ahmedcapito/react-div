1c485e62a29a915c0a2601ca321d7b0991344c32
Ahmedcapito <163877218+Ahmedcapito@users.noreply.github.com>
2024-08-03 09:24:51 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <section className="container">
+      <h1>ahmed saber</h1>
+      <p>Lorem ipsum, dolor sit  consectetur  . , ? Tempore, !</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
